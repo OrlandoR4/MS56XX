0ac3badd8e16ccf884dd8af02fb8a767d2fd2eab
moonman <74813604+abhignay@users.noreply.github.com>
2022-02-15 21:48:49 +0530

    added second order temperature compesation

--- src/MS56XX.cpp
+++ src/MS56XX.cpp
@@ -136,18 +136,49 @@ bool MS56XX::doBaro(bool doAltitude){ //RETURNS TRUE IF NEW DATA IS CALCULATED
     return true;
 }
 
+float offset;
+float sens;
+
+// second degree temperature correction variables
+float temperature2;
+float offset2;
+float sens2;
+
 void MS56XX::calculateTemperature(){
     dT_temp_diff = float(d2_temperature) - float(c5_temp_ref) * float(1 << 8);
     float actual_temperature = 2000.0f + dT_temp_diff * float(c6_temp_coef_sens) / float(1 << 23);
-    
-    //Might add second order temperature compensation for less than 20C temperatures
+    temperature = actual_temperature; 
 
-    temperature = actual_temperature/100.0f; // Degrees Centigrade
+    //second order temperature compensation for optimum accuracy, pg.9 in datasheet
+    if (temperature < 2000){ // temperature lower than 20C
+        // temperature lower than 20C
+        temperature2 = pow(dT_temp_diff, 2) / 2147483648; // 2147483648 = 2^31
+        offset2 = 61 - pow((temperature - 2000), 2) / 16;
+        sens2 = 2 * pow((temperature - 2000), 2);
+
+        // very low temperature correction
+        if (temperature < -1500){ // -15C 
+            offset2 = offset2 + 15 * pow((temperature + 1500), 2);
+            sens2 = sens2 + 8 * pow((temperature + 1500), 2);
+        }
+    }
+    else {
+        // if temperature greater than 20C nothing needs to be done
+        temperature2 = 0;
+        offset2 = 0;
+        sens2 = 0;
+    }
+
+    temperature -= temperature2;
+    temperature = temperature / 100.0f; // Degrees centigrade
+
+    offset -= offset2;
+    sens -= sens2;
 }
 
 void MS56XX::calculateCompensatedPressure(){
-    float offset = float(c2_pres_off) * float(1 << (17 - var_const)) + float(c4_temp_coef_pres_off) * float(dT_temp_diff)/float(1 << (6 + var_const));
-    float sens = float(c1_pres_sens) * float(1 << (16 - var_const)) + float(c3_temp_coef_pres_sens) * float(dT_temp_diff)/float(1 << (7 + var_const));
+    offset = float(c2_pres_off) * float(1 << (17 - var_const)) + float(c4_temp_coef_pres_off) * float(dT_temp_diff)/float(1 << (6 + var_const));
+    sens = float(c1_pres_sens) * float(1 << (16 - var_const)) + float(c3_temp_coef_pres_sens) * float(dT_temp_diff)/float(1 << (7 + var_const));
     float comp_pressure = ( (float(d1_pressure) * sens / float(1 << 21)) - offset)/float(1 << 15);
 
     pressure = comp_pressure; // Pascals
